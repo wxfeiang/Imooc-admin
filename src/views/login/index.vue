cbbdedc4b856a7ba2e9d11d874f1fd30c67e349b
wxfeiang <wxfeiang@qq.com>
2022-01-03 22:03:27 +0800

    feat(components): 表单提交数据 样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="bg_box">
     <h3 class="title">后台管理系统</h3>
-    <el-from label-width="120px" :model="loginFrom" ref="loginFrom">
-      <el-form-item>
+    <el-from
+      label-width="120px"
+      :model="loginFrom"
+      :rules="loginRules"
+      ref="loginFrom"
+    >
+      <el-form-item prop="username">
         <el-input v-model="loginFrom.username" placeholder="请输入用户名">
           <template #prefix>
             <el-icon class="el-input__icon"><Avatar /></el-icon>
@@ -10,7 +15,7 @@
         </el-input>
       </el-form-item>
 
-      <el-form-item>
+      <el-form-item prop="password">
         <el-input v-model="loginFrom.password" placeholder="请输入密码">
           <template #prefix>
             <el-icon class="el-input__icon"><Search /></el-icon>
@@ -27,18 +32,39 @@
 <script setup>
 import { Avatar, Search } from '@element-plus/icons-vue'
 import { ref } from 'vue'
-//  TODO
+import { validatePassword } from './rules'
+//  登陆
 const loginFrom = ref({
-  username: '',
-  password: ''
+  username: '22',
+  password: '222'
 })
-
-// const username = ref('')
-// const password = ref('')
+// loginRules
+const loginRules = ref({
+  username: [
+    {
+      required: true,
+      message: 'Please input Activity name',
+      trigger: 'bulr'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: 'Please input Activity name',
+      validator: validatePassword()
+    }
+  ]
+})
 </script>
 
 <style>
 .bg_box {
+  width: 40%;
+  margin: 10% auto 0;
   background-color: #f5f5f5;
+  padding: 30px;
+}
+.el-button {
+  width: 100%;
 }
 </style>
