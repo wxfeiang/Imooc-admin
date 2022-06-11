4888e4921d3f20ec1696040e077d728bf5c6cda5
wxfeiang <wxfeiang@qq.com>
2022-06-11 17:16:23 +0800

    fix(测试关闭i suisu): fix(测试关闭isu): ceshi guanbi isu
    ISSUES CLOSED: #1
    ISSUES CLOSED: #1
    
    ISSUES CLOSED: #1

--- .cz-config.js
+++ .cz-config.js
@@ -2,9 +2,9 @@
 module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
-    { value: 'feat', name: 'feat:     新增功能' },
-    { value: 'fix', name: 'fix:      修复 bug' },
-    { value: 'docs', name: 'docs:     文档变更' },
+    { value: 'feat', name: 'feat:新增功能' },
+    { value: 'fix', name: 'fix: 修复 bug' },
+    { value: 'docs', name: 'docs:文档变更' },
     {
       value: 'style',
       name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）'
@@ -13,19 +13,19 @@ module.exports = {
       value: 'refactor',
       name: 'refactor: 代码重构（不包括 bug 修复、功能新增）'
     },
-    { value: 'perf', name: 'perf:     性能优化' },
-    { value: 'test', name: 'test:     添加、修改测试用例' },
+    { value: 'perf', name: 'perf: 性能优化' },
+    { value: 'test', name: 'test:添加、修改测试用例' },
     {
       value: 'build',
       name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
     },
-    { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
+    { value: 'ci', name: 'ci: 修改 CI 配置、脚本' },
     {
       value: 'chore',
       name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）'
     },
-    { value: 'revert', name: 'revert:   回滚 commit' },
-    { value: 'del', name: 'del:      删除代码/文件' }
+    { value: 'revert', name: 'revert: 回滚 commit' },
+    { value: 'del', name: 'del: 删除代码/文件' }
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
@@ -88,9 +88,9 @@ module.exports = {
 
   // subject 限制长度
   subjectLimit: 100,
-  breaklineChar: '|' // 支持 body 和 footer
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true,
+  breaklineChar: '|', // 支持 body 和 footer
+  footerPrefix: 'ISSUES CLOSED:',
+  askForBreakingChangeFirst: true
 
-  // 测试关闭
+  // 测试关闭is
 }
